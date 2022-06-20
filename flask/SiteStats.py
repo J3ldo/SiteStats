b30d7de396d3673a5d31a7d92617fe5f06d06a06
J3ldo <97303347+J3ldo@users.noreply.github.com>
2022-06-20 21:29:15 +0200

    Create the module

---
+++ flask/SiteStats.py
@@ -0,0 +1,45 @@
+from flask import request
+
+requests_made = 0
+unique_made = 0
+amount_visited = {}
+amount_unique_visited = {}
+data = {}
+
+requests_processed = 0
+requests_failed = 0
+unique_processed = 0
+unique_failed = 0
+processed_data = {}
+def start(app):
+  @app.before_request
+  def not_important():
+     global requests_made, unique_made, requests_failed, requests_processed, unique_processed, processed_data, unique_failed
+
+     requests_made += 1
+     requests_failed += 1
+     try: amount_visited[request.url] += 1
+     except: amount_visited[request.url] = 1
+
+     if data.get(request.remote_addr) is None:
+         unique_made += 1
+         unique_failed += 1
+         data[request.remote_addr] = {request.url: 1}
+         amount_unique_visited[request.url] = 1
+
+     elif data.get(request.remote_addr).get(request.url) is None:
+         amount_unique_visited[request.url] = 1
+
+
+  @app.after_request
+  def not_important2(_):
+      global requests_made, unique_made, requests_failed, requests_processed, unique_processed, processed_data, unique_made, unique_failed
+
+      requests_processed += 1
+      requests_failed -= 1
+      if processed_data.get(request.remote_addr) is None:
+          unique_processed += 1
+          unique_failed -= 1
+          processed_data[request.remote_addr] = 1
+
+      return _
